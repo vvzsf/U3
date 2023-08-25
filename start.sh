5170f40d738ab60573736e7689cef70bd6eff075
ghalebishal <134343851+ghalebishal@users.noreply.github.com>
2023-08-25 21:59:08 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Jakeedot/URLShortner.git /URLShortener
+git clone https://github.com/ghalebishal/URLShorterner.git /URLShortener
 cd /URLShortener
 pip3 install -r requirements.txt
 echo "Starting Bot...."
