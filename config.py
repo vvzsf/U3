4855da95ecadea1f3ebbf5b1aff57466dc5f1656
ghalebishal <134343851+ghalebishal@users.noreply.github.com>
2023-08-26 16:01:00 +0545

    Update config.py

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "18522520"))
-API_HASH = os.environ.get("API_HASH", "7ae9da3e228f89c1557d3d6de9e635b3")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6568201991:AAF3j-weeNbXh7YpfycHAOSqZqHtvacEITI")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "5868126738").split(",") if i.strip()] 
+API_ID = int(os.getenv("API_ID", "27318489"))
+API_HASH = os.environ.get("API_HASH", "6e87be2c71039c0c6f2077266f9827c2")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6638166308:AAEauhWVObw1mCglDLAFF1PzKC51v2eJdLA")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "5979184565").split(",") if i.strip()] 
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "cluster0")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Bishalghale:Bishalghale@cluster0.z7gtude.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "5868126738")) 
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://ghale1040:@Ghale1040@cluster0.56rnlmr.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5979184565")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001528714984")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Kajilinks_Official") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001769312503")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Kajilinks_official") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
 LINK_BYPASS = "False"
