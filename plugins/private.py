5a6c11648520bb83cce1daf2523c9d6e77a222b0
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-31 13:58:05 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 channel = UPDATE_CHANNEL
 
-ft = f"You have not joined in my channel. Please join @Kajilinks_Official to use this bot."
+ft = f"You have not joined in my channel. Please join @urlshortx1 to use this bot."
 
 
 # Private Chat
