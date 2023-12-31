cb8154559d164a5bf035b1aa1b48410816cafdeb
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-31 17:16:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,53 +28,38 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__" :
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            "shortener",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins"),
-        )
-
+    plugins = dict(
+        root="plugins"
+    )
+    dkbotz = Client(
+        "Mdisk-Pro",
+        bot_token=BOT_TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+    
     async def start(self):
-
-        temp.START_TIME = datetime.datetime.now()
-        await super().start()
-
-        if UPDATE_CHANNEL:
-            try:
-                self.invite_link = await self.create_chat_invite_link(UPDATE_CHANNEL)
-            except Exception:
-                logging.error(
-                    f"Make sure to make the bot in your update channel - {UPDATE_CHANNEL}"
-                )
-                sys.exit(1)
-
         me = await self.get_me()
         self.owner = await self.get_users(int(OWNER_ID))
-        self.username = f"@{me.username}"
+        self.username = f'@{me.username}'
         temp.BOT_USERNAME = me.username
         temp.FIRST_NAME = me.first_name
         if not await db.get_bot_stats():
             await db.create_stats()
-
         banned_users = await filter_users({"banned": True})
         async for user in banned_users:
             temp.BANNED_USERS.append(user["user_id"])
+        logging.info(LOG_STR)
+        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @DKBOTZ')
+        logging.info('Bot started')
 
-        await set_commands(self)
 
-        await broadcast_admins(self, "** Bot started successfully **")
-        logging.info("Bot started")
+    dkbotz.run()
 
-        if WEB_SERVER:
-            await create_server()
-            logging.info("Web server started")
-            logging.info("Pinging server")
-
-    async def stop(self):
-        await broadcast_admins(self, "** Bot Stopped Bye **")
+async def stop(self, *args):
         await super().stop()
-        logging.info("Bot Stopped Bye")
+        logging.info("Bot stopped. Bye.")
+
+#dkbotz = Bot()
+#dkbotz.run()
