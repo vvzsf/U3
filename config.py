369714d0c6c51ed9307f3626927cbb49e8ff9ff2
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-31 15:21:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "BOT_TOKEN")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "ADMINS").split(",") if i.strip()] 
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "cluster0")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://kajilinks:kajilinks@cluster0.pae9hcd.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "5979184565")) 
+DATABASE_URL = os.getenv("DATABASE_URL", "") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001769312503")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "UPDATE_CHANNEL") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
 LINK_BYPASS = "False"
